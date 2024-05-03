bffa20548393e1f3cc228f41316151f4268e9cc3
Tomasz Żelawski <tzelawski@gmail.com>
2024-05-03 23:48:33 +0200

    Squashed commit of the following:
    commit d5a36d54de3731f8cfc7ce96bd6c9843668097f7
    Author: Tomasz Żelawski <tzelawski@gmail.com>
    Date:   Fri May 3 23:47:46 2024 +0200
    
        remove logging
    
    commit e23804d97a4a19c78a012bb0eed81037ac4f7afb
    Merge: 821460e 2e181ad
    Author: Tomasz Żelawski <tzelawski@gmail.com>
    Date:   Fri May 3 23:31:55 2024 +0200
    
        Merge branch 'main' into @tjzel/fix-framerate
    
    commit 821460e2b27e603d6cd2c5ec05c7c3c54ee0cf5d
    Author: Tomasz Żelawski <tzelawski@gmail.com>
    Date:   Fri May 3 23:31:06 2024 +0200
    
        init

--- real-pendulum/app/framerate.ts
+++ real-pendulum/app/framerate.ts
@@ -3,25 +3,65 @@
 export function getFrameDuration() {
   return new Promise<number>((resolve) => {
     let oldTimestamp = performance.now();
-    const length = 10;
-    let totalTime = 0;
+    const length = 16;
+    let values = new Array(length).fill(0);
     let counter = 0;
     requestAnimationFrame(callback);
 
     function callback() {
       const newTimestamp = performance.now();
       const difference = newTimestamp - oldTimestamp;
-      console.log("difference", difference);
       oldTimestamp = newTimestamp;
-      totalTime += difference;
-      if (counter < length) {
+      values[counter] = difference;
+
+      if (++counter < length) {
         requestAnimationFrame(callback);
-        counter++;
       } else {
-        const meanFpsValue = Math.round(totalTime / length);
-        console.log(`Mean FPS: ${meanFpsValue}`);
-        resolve(meanFpsValue);
+        const meanFrameDuration = calculateFrameDuration(values);
+        if (meanFrameDuration > 0) {
+          resolve(meanFrameDuration);
+        } else {
+          values = new Array(length).fill(0);
+          counter = 0;
+          requestAnimationFrame(callback);
+        }
       }
     }
   });
+}
+
+function calculateFrameDuration(values: number[]) {
+  let extremesPresent = true;
+  let meanFrameDuration = 0;
+
+  while (extremesPresent) {
+    extremesPresent = false;
+    meanFrameDuration = values.reduce((a, b) => a + b, 0) / values.length;
+
+    const stdDev = Math.sqrt(
+      values.reduce((a, b) => a + Math.pow(b - meanFrameDuration, 2), 0) /
+        values.length
+    );
+
+    const furthestValue = values.reduce((a, b) =>
+      Math.abs(b - meanFrameDuration) > Math.abs(a - meanFrameDuration) ? b : a
+    );
+
+    const twoSigma = stdDev * 2;
+    const deviation = Math.abs(furthestValue - meanFrameDuration);
+
+    if (deviation > twoSigma) {
+      const index = values.indexOf(furthestValue);
+      if (index !== -1) {
+        values.splice(index, 1);
+        extremesPresent = true;
+      }
+    }
+
+    if (values.length === 1) {
+      return 0;
+    }
+  }
+
+  return meanFrameDuration;
 }
