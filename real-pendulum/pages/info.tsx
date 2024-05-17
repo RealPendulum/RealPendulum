5f9df3fc62db522d7d6e85b51c4a9fac83397cc9
Tomasz Żelawski <tzelawski@gmail.com>
2024-05-17 09:59:44 +0200

    WIP

--- real-pendulum/pages/info.tsx
+++ real-pendulum/pages/info.tsx
@@ -2,9 +2,15 @@ import { useState, type ReactNode } from "react";
 import { Button } from "@mui/material";
 import { PendulumType, PendulumContainer } from "@/app/pendulum";
 import "@/app/globals.css";
-import Link from "next/link";
 import NavigationBar from "@/app/navigation";
 import Urls from "@/app/urls";
+import {
+  ArrowBackIos,
+  ArrowForwardIos,
+  Circle,
+  CircleOutlined,
+} from "@mui/icons-material";
+import Image from "next/image";
 
 export default function Info() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -14,7 +20,7 @@ export default function Info() {
       <NavigationBar currentSiteUrl={Urls.infoURL} />
       <div className="flex-grow overflow-auto bg-gray-200">
         <div className="container center mx-auto p-5">
-          <Slide />
+          <Slide setCurrentSlide={setCurrentSlide} />
         </div>
       </div>
       <div className="p-5">
@@ -36,7 +42,7 @@ export default function Info() {
 }
 
 const Slides: Slide[] = [
-  { Slide: Slide0, nextText: "Yes" },
+  { Slide: Slide0 },
   { Slide: Slide1 },
   { Slide: Slide2 },
   { Slide: Slide3 },
@@ -62,27 +68,23 @@ function NavigationPanel({ currentSlide, setCurrentSlide }: NavigationProps) {
             <Button
               key={index}
               color="primary"
+              size="small"
               onClick={() => setCurrentSlide(index)}
             >
-              🟡
+              <Circle />
             </Button>
           ) : (
             <Button
               key={index}
               color="primary"
+              size="small"
               onClick={() => setCurrentSlide(index)}
             >
-              ⚪
+              <CircleOutlined />
             </Button>
           )
         )}
       </div>
-      <Link
-        href={"/"}
-        className="m-3 flex h-24 w-24 items-center justify-center rounded-full bg-blue-600 hover:bg-blue-800 transition duration-200 hover:scale-125"
-      >
-        {"Go back"}
-      </Link>
     </div>
   );
 }
@@ -98,7 +100,9 @@ function NavigationArrows({ currentSlide, setCurrentSlide }: NavigationProps) {
           color="primary"
           onClick={() => setCurrentSlide(currentSlide - 1)}
         >
-          {prevText}
+          {/* {prevText}
+           */}
+          <ArrowBackIos />
         </Button>
       )}
       {currentSlide < Slides.length - 1 && (
@@ -107,18 +111,34 @@ function NavigationArrows({ currentSlide, setCurrentSlide }: NavigationProps) {
           color="primary"
           onClick={() => setCurrentSlide(currentSlide + 1)}
         >
-          {nextText}
+          {/* {nextText}
+           */}
+          <ArrowForwardIos />
         </Button>
       )}
     </div>
   );
 }
 
-function Slide0() {
+function Slide0({
+  setCurrentSlide,
+}: {
+  setCurrentSlide: (slide: number) => void;
+}) {
   const texts = ["Hi there!", "Let's talk pendulums.", "Are you ready?"];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
+      <Button
+        className="mt-4 bg-blue-400"
+        variant="contained"
+        color="primary"
+        size="large"
+        // @ts-ignore
+        onClick={() => setCurrentSlide((value) => value + 1)}
+      >
+        Yes
+      </Button>
     </SlideTemplate>
   );
 }
@@ -203,6 +223,13 @@ function Slide4() {
     "Let's look back at the real pendulum. Let's even go further and compare it with an approximation. Do you see the difference?",
     "Which one is real?",
   ];
+  const [answer, setAnswer] = useState<string | null>(null);
+  const correctAnswerTexts = [
+    "You are correct - seems like luck is on your side.",
+  ];
+  const wrongAnswerTexts = [
+    "You were wrong - but don't worry, it's just bad luck.",
+  ];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
@@ -232,18 +259,22 @@ function Slide4() {
           </div>
         </div>
       </Figure>
+      <AnswerButtons answer={answer} setAnswer={setAnswer} />
+      <AnswerTexts
+        answer={answer}
+        correctText={correctAnswerTexts}
+        wrongText={wrongAnswerTexts}
+      />
     </SlideTemplate>
   );
 }
 
 function Slide5() {
   const texts = [
-    "You are correct - seems like luck is on your side.",
-    "You were wrong - but don't worry, it's just bad luck.",
     "I should explain first what do I mean by the real pendulums and the approximated ones.",
     "Aren't both approximated, since they are digital.",
     "Well - yes, but that's a completely other topic.",
-    "I hope you had some physics courses throughout your education, because I'm not going to explain all {mechanika klasyczna/kinematyka} here.",
+    "I hope you had some physics courses throughout your education, because I'm not going to explain all kinematics here.",
     "Let's look at this pretty figure of a pendulum.",
   ];
   return (
@@ -256,27 +287,43 @@ function Slide5() {
 
 function Slide6() {
   const texts = [
-    "We obviously assume, that the string/rod/bar/whatever is there that connects the weight to the anchor of a pendulum is weightless and completely stiff.",
+    "We obviously assume the string that connects the weight to the anchor of a pendulum is weightless and completely stiff.",
     "What do we mean by completely stiff? Well, basically, that we can totally forget about it.",
     "Therefore we get a simple equation for the outcome force that is applied to the weight.",
   ];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
-      <Figure text="PLACEHOLDER: AN EQUATION FOR FORCE" />
+      <Figure>
+        <Image
+          src="/assets/pendulum-force.png"
+          alt="An equation for pendulum's force"
+          width={400}
+          height={400}
+          className="border-4 border-black rounded-lg"
+        />
+      </Figure>
     </SlideTemplate>
   );
 }
 
 function Slide7() {
   const texts = [
-    "What we know from physics, is that force is a derivative of momentum both on mass and speed.",
+    "What we know from physics, is that force is a time derivative of momentum both on mass and speed.",
     "Here we obviously assume, that the mass is constant.",
   ];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
-      <Figure text="PLACEHOLDER: AN EQUATION FOR FORCE WITH DERIVATIVES" />
+      <Figure>
+        <Image
+          src="/assets/force-derivative.png"
+          alt="An equation for force with derivatives"
+          width={400}
+          height={400}
+          className="border-4 border-black rounded-lg"
+        />
+      </Figure>
     </SlideTemplate>
   );
 }
@@ -286,17 +333,38 @@ function Slide8() {
     "So we have linked the acceleration to the speed. That's not really a discovery.",
     "Then, it won't be any other discovery if we link speed to position.",
   ];
+  const moreTexts = ["Now all we need is to substitute our force."];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
-      <Figure text="PLACEHOLDER: EN EQUATION FOR FORCE WITH SECOND DERIVATIVE TO GET LOCATION" />
+      <Figure>
+        <Image
+          src="/assets/last-derivative.png"
+          alt="An equation for acceleration with derivatives"
+          width={400}
+          height={400}
+          className="border-4 border-black rounded-lg"
+        />
+      </Figure>
+      <div className="m-5" />
+      <TextFromArray texts={moreTexts} />
+      <Figure>
+        <Image
+          src="/assets/substitute.png"
+          alt="An equation for pendulum's location"
+          width={400}
+          height={400}
+          className="border-4 border-black rounded-lg"
+        />
+      </Figure>
     </SlideTemplate>
   );
 }
 
 function Slide9() {
   const texts = [
-    "And there we go. That's the location. That's the pendulum. This is the finest way to describe it we know of (as long as you don't include more complicated physics here).",
+    "And there we go. That's the location. That's the pendulum.",
+    "This is the finest way to describe it we know of - as long as you don't include more complicated physics here.",
     "We just need to integrate it twice, and we get the function for location.",
     "There's one problem with that though. This equation is hard. Very hard.",
     "I must admit here, I forgot a bit of my physics course and I don't remember if it's impossible to find a nice little function that is the solution here, of if we haven't found it yet.",
@@ -318,19 +386,27 @@ function Slide10() {
     "This is the real pain here. I wonder how to make it simpler.",
     "Let's do a wild guess - let's replace the sin function with just its argument, why not.",
     "We can check, that for the small values, it's really close.",
-    "(Those who know a bit of maths know that it's simple Taylor's expansion).",
+    "Those who know a bit of maths know that it's a simple Taylor's expansion.",
   ];
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
-      <Figure text="PLACEHOLDER: TABLE COMPARING SINX AND X" />
+      <Figure>
+        <Image
+          src="/assets/x-sinx-comparison.png"
+          alt="A table comparing sin(x) and x"
+          width={400}
+          height={400}
+          className="border-4 border-black rounded-lg"
+        />
+      </Figure>
     </SlideTemplate>
   );
 }
 
 function Slide11() {
   const texts = [
-    "So, if we agree that a small error here would be, say, 5%, we see that this approximation would be good to up to {number} degrees.",
+    "So, if we agree that a small error here would be, say, five percent, we see that this approximation would be good to up to around 0.6 radians or 34 degrees.",
     "And as it turns out now, this is really easy to solve now, provided one has some knowledge of differential equation.",
     "Solution is as goes:",
   ];
@@ -360,37 +436,52 @@ function Slide13() {
     "Remember when I said about forgetting the limits of the approximation? This is because, in the digital world, people often misuse the pendulums made with the approximate equation, using the angles far beyond what is reasonable.",
     "Could you guess which pendulum is true now?",
   ];
+  const correctAnswerTexts = ["That was much easier, wasn't it?"];
+  const wrongAnswerTexts = [
+    "Well, you're wrong, but I'll just assume you didn't pay close attention.",
+  ];
+  const [answer, setAnswer] = useState<string | null>(null);
   return (
     <SlideTemplate>
       <TextFromArray texts={texts} />
       <Figure text="PLACEHOLDER: TWO PENDULUMS, ONE REAL, ONE APPROXIMATED">
-        <PendulumContainer
-          isWaitingToStart={false}
-          color="lightgreen"
-          pendulumParams={{
-            initialAngle: 0.45 * Math.PI,
-            initialSpeed: 0,
-            pendulumType: PendulumType.ODE,
-          }}
-        />
-        <PendulumContainer
-          isWaitingToStart={false}
-          color="aquamarine"
-          pendulumParams={{
-            initialAngle: 0.45 * Math.PI,
-            initialSpeed: 0,
-            pendulumType: PendulumType.Approximation,
-          }}
-        />
+        <div className="flex flex-row justify-between w-64">
+          <div>
+            <PendulumContainer
+              isWaitingToStart={false}
+              color="lightgreen"
+              pendulumParams={{
+                initialAngle: 0.45 * Math.PI,
+                initialSpeed: 0,
+                pendulumType: PendulumType.ODE,
+              }}
+            />
+          </div>
+          <div>
+            <PendulumContainer
+              isWaitingToStart={false}
+              color="aquamarine"
+              pendulumParams={{
+                initialAngle: 0.45 * Math.PI,
+                initialSpeed: 0,
+                pendulumType: PendulumType.Approximation,
+              }}
+            />
+          </div>
+        </div>
       </Figure>
+      <AnswerButtons answer={answer} setAnswer={setAnswer} />
+      <AnswerTexts
+        answer={answer}
+        correctText={correctAnswerTexts}
+        wrongText={wrongAnswerTexts}
+      />
     </SlideTemplate>
   );
 }
 
 function Slide14() {
   const texts = [
-    "That was much easier was it?",
-    "Well, you're wrong, but I'll just assume you didn't pay close attention.",
     "What is interesting to me is: do people mistake approximations, especially outside their boundaries, for the real behavior of a real pendulum?",
     "Please let me confirm that, playing <the game>.",
   ];
@@ -418,14 +509,74 @@ function Figure({ children, text }: { children?: ReactNode; text?: string }) {
   );
 }
 
-function TextFromArray({ texts }: { texts: string[] }) {
+function TextFromArray({ texts, color }: { texts: string[]; color?: string }) {
   return (
-    <div className="text-center text-black">
+    <div
+      className={
+        color
+          ? `text-center ${color} font-semibold leading-10 text-xl max-w-[65vh]`
+          : "text-center text-black font-semibold leading-10 text-xl max-w-[65vh]"
+      }
+    >
       {texts.map((text, index) => (
-        <div key={index}>{text}</div>
+        <div className="m-2" key={index}>
+          {text}
+        </div>
       ))}
     </div>
   );
+}
+
+function AnswerButtons({
+  answer,
+  setAnswer,
+}: {
+  answer: string | null;
+  setAnswer: (answer: string) => void;
+}) {
+  return (
+    <div className="flex flex-row justify-center">
+      <Button
+        className="m-5 bg-blue-400"
+        variant={`${answer === "correct" ? "outlined" : "contained"}`}
+        color="primary"
+        size="large"
+        disabled={answer !== null}
+        onClick={() => setAnswer("correct")}
+      >
+        Left
+      </Button>
+      <Button
+        className="m-5 bg-blue-400"
+        variant={`${answer === "wrong" ? "outlined" : "contained"}`}
+        color="primary"
+        size="large"
+        disabled={answer !== null}
+        onClick={() => setAnswer("wrong")}
+      >
+        Right
+      </Button>
+    </div>
+  );
+}
+
+function AnswerTexts({ answer, correctText, wrongText }: AnswerTextsProps) {
+  return (
+    <div>
+      {answer === "correct" && (
+        <TextFromArray texts={correctText} color="text-green-600" />
+      )}
+      {answer === "wrong" && (
+        <TextFromArray texts={wrongText} color="text-red-600" />
+      )}
+    </div>
+  );
+}
+
+interface AnswerTextsProps {
+  answer: string | null;
+  correctText: string[];
+  wrongText: string[];
 }
 
 interface NavigationProps {
@@ -436,4 +587,5 @@ interface NavigationProps {
 interface Slide {
   Slide: React.FC;
   nextText?: string;
+  setCurrentSlide?: (slide: number) => void;
 }
